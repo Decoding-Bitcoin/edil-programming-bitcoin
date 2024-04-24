57262d4a15b4e60b33647ee808d53cce50e0777a
Edil Medeiros <jose.edil@gmail.com>
2024-04-24 11:27:39 -0300

    Add script evaluation
    TODO: implement opcode functions

--- pybitcoin/script.py
+++ pybitcoin/script.py
@@ -1,10 +1,14 @@
-from pybitcoin import opcodes
+from logging import getLogger
+
+from pybitcoin.opcodes import OP_CODE_FUNCTIONS, OP_CODE_NAMES
 from pybitcoin.util import (
     encode_varint,
     read_varint,
     little_endian_to_int,
     int_to_little_endian
 )
+
+LOGGER = getLogger(__name__)
 
 
 class Script:
@@ -15,6 +19,68 @@ class Script:
         else:
             self.cmds = cmds
 
+    def __repr__(self):
+        result = []
+        for cmd in self.cmds:
+            if type(cmd) == int:
+                if OP_CODE_NAMES.get(cmd):
+                    name = OP_CODE_NAMES.get(cmd)
+                else:
+                    name = 'OP_[{}]'.format(cmd)
+                result.append(name)
+            else:
+                result.append(cmd.hex())
+        return ' '.join(result)
+
+    def __add__(self, other):
+        return Script(self.cmds + other.cmds)
+
+    def evaluate(self, z):
+        cmds = self.cmds[:]   # copy cmds list, it will mutate
+        stack = []
+        altstack = []
+        while len(cmds) > 0:
+            cmd = cmds.pop(0)
+            if type(cmd) == int:
+                operation = OP_CODE_FUNCTIONS[cmd]
+
+                # OP_IF and OP_NOTIF
+                if cmd in (0x63,0x64):
+                    if not operation(stack, cmds):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+
+                # OP_TOALTSTACK and OP_FROMALTSTACK
+                elif cmd in (0x6b, 0x6c):
+                    if not operation(stack, altstack):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+
+                # OP_CHECKSIG, OP_CHECKSIGVERIFY,
+                # OP_CHECKMULTISIG, OP_CHECKMULTISIGVERIFY
+                elif cmd in (0xac, 0xad, 0xae, 0xaf):
+                    if not operation(stack, z):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+
+                else:
+                    if not operation(stack):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+
+            # element (not opcode), push to stack
+            else:
+                stack.append(cmd)
+
+        # if stack is empty at the end, script fails
+        if len(stack) == 0:
+            return False
+        # if stack's top element is empty (zero), script fails
+        if stack.pop == b'':
+            return False
+        # otherwise, scrip succeeds
+        return True
+    
     @classmethod
     def parse(cls, s):
         length = read_varint(s)
