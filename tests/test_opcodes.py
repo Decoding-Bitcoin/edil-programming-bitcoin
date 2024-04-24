ac55e0de8624bd97d30b1185410004e85d0f0fb8
Edil Medeiros <jose.edil@gmail.com>
2024-04-24 13:46:16 -0300

    Refactor tests

--- tests/test_opcodes.py
+++ tests/test_opcodes.py
@@ -4,85 +4,55 @@ from pybitcoin.opcodes import *
 
 class OpcodesTest(TestCase):
 
-    def test_op_0(self):
-        stack = []
-        expected = [b'']
-        self.assertTrue(op_0(stack))
-        self.assertEqual(stack, expected)
+    def _test_opcode(self, opcode, stack, expected_stack):
+        self.assertTrue(OP_CODE_FUNCTIONS[opcode](stack))
+        self.assertEqual(stack, expected_stack)
 
-        stack = [0, 1, 2]
-        expected = [0, 1, 2, b'']
-        self.assertTrue(op_0(stack))
-        self.assertEqual(stack, expected)
+    def test_op_0(self):
+        self._test_opcode(0x00, [], [b''])
+        self._test_opcode(0x00, [0,1,2], [0,1,2,b''])
 
     def test_op_1negate(self):
-        stack = []
-        expected = [b'\x81']
-        self.assertTrue(op_1negate(stack))
-        self.assertEqual(stack, expected)
-
-        stack = [0, 1, 2]
-        expected = [0, 1, 2, b'\x81']
-        self.assertTrue(op_1negate(stack))
-        self.assertEqual(stack, expected)
+        self._test_opcode(0x4f, [], [b'\x81'])
+        self._test_opcode(0x4f, [0,1,2], [0,1,2,b'\x81'])
 
     def test_op_1_to_15(self):
         for i in range(1,17):
             with self.subTest(i=i):
                 opcode = i + 0x50
-
-                stack = []
-                expected = [i.to_bytes(1, 'little')]
-                self.assertTrue(OP_CODE_FUNCTIONS[opcode](stack))
-                self.assertEqual(stack, expected)
-
-                stack = [0, 1, 2]
-                expected = [0, 1, 2, i.to_bytes(1, 'little')]
-                self.assertTrue(OP_CODE_FUNCTIONS[opcode](stack))
-                self.assertEqual(stack, expected)
+                self._test_opcode(opcode, [], [i.to_bytes(1, 'little')])
+                self._test_opcode(opcode, [0,1,2], [0,1,2,i.to_bytes(1, 'little')])
 
     def test_op_nop(self):
-        opcode = 0x61
-        stack = []
-        expected = []
-        self.assertTrue(OP_CODE_FUNCTIONS[opcode](stack))
-        self.assertEqual(stack, expected)
+        self._test_opcode(0x61, [], [])
 
     def test_op_dup(self):
         stack = []
         self.assertFalse(op_dup(stack))
 
-        stack = [0,1,2]
-        self.assertTrue(op_dup(stack))
-        self.assertEqual(stack, [0,1,2,2])
+        self._test_opcode(0x76, [0,1,2], [0,1,2,2])
 
     def test_op_hash160(self):
         stack = []
         self.assertFalse(op_hash160(stack))
 
-        stack = [b'\x00']
-        expected_stack = [bytes.fromhex('9f7fd096d37ed2c0e3f7f0cfc924beef4ffceb68')]
-        self.assertTrue(op_hash160(stack))
-        self.assertEqual(stack, expected_stack)
-
-        stack = [2, 1, b'\x00']
-        expected_stack = [2, 1, bytes.fromhex('9f7fd096d37ed2c0e3f7f0cfc924beef4ffceb68')]
-        self.assertTrue(op_hash160(stack))
-        self.assertEqual(stack, expected_stack)
+        self._test_opcode(opcode=0xa9,
+                          stack=[b'\x00'],
+                          expected_stack=[bytes.fromhex('9f7fd096d37ed2c0e3f7f0cfc924beef4ffceb68')])
+        self._test_opcode(opcode=0xa9,
+                          stack=[2, 1, b'\x00'],
+                          expected_stack=[2, 1, bytes.fromhex('9f7fd096d37ed2c0e3f7f0cfc924beef4ffceb68')])
 
     def test_op_hash256(self):
         stack = []
         self.assertFalse(op_hash256(stack))
 
-        stack = [b'\x00']
-        expected_stack = [bytes.fromhex('1406e05881e299367766d313e26c05564ec91bf721d31726bd6e46e60689539a')]
-        self.assertTrue(op_hash256(stack))
-        self.assertEqual(stack, expected_stack)
-
-        stack = [2, 1, b'\x00']
-        expected_stack = [2, 1, bytes.fromhex('1406e05881e299367766d313e26c05564ec91bf721d31726bd6e46e60689539a')]
-        self.assertTrue(op_hash256(stack))
-        self.assertEqual(stack, expected_stack)
+        self._test_opcode(0xaa,
+                          stack = [b'\x00'],
+                          expected_stack = [bytes.fromhex('1406e05881e299367766d313e26c05564ec91bf721d31726bd6e46e60689539a')])
+        self._test_opcode(0xaa,
+                          stack = [2, 1, b'\x00'],
+                          expected_stack = [2, 1, bytes.fromhex('1406e05881e299367766d313e26c05564ec91bf721d31726bd6e46e60689539a')])
 
 
 if __name__ == '__main__':
